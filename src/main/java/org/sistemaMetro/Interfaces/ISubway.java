aa63661b9fcf46e67fe14832d3cdf42ce30096c7
Vicente Aninat <vicente.aninat@usach.cl>
2024-06-13 22:20:16 -0400

    Se añadió el metodo de añadir trenes a una red como parte de una interfaz para que la clase herede de esta

--- src/main/java/org/sistemaMetro/Interfaces/ISubway.java
+++ src/main/java/org/sistemaMetro/Interfaces/ISubway.java
@@ -1,4 +1,9 @@
 package org.sistemaMetro.Interfaces;
 
+import org.sistemaMetro.clases.Subway;
+import org.sistemaMetro.clases.Train;
+
 public interface ISubway {
+
+    void subwayAddTrain(Subway subway, Train train);
 }
