f63f4eaec0497632f754c19925ca36ed54006f7d
Vicente Aninat <vicente.aninat@usach.cl>
2024-06-14 23:29:08 -0400

    Se añadieron metodos requeridos a la clase subway

--- src/main/java/org/sistemaMetro/Interfaces/ISubway.java
+++ src/main/java/org/sistemaMetro/Interfaces/ISubway.java
@@ -2,8 +2,33 @@ package org.sistemaMetro.Interfaces;
 
 import org.sistemaMetro.clases.Subway;
 import org.sistemaMetro.clases.Train;
+import org.sistemaMetro.clases.Line;
+import org.sistemaMetro.clases.Driver;
+
+import java.util.ArrayList;
 
 public interface ISubway {
+    /**
+     * Agrega varios trenes a la red de metro
+     *
+     * @param subway    red de metro a la que se agregarán los trenes
+     * @param trainList lista de trenes que se agregarán al metro
+     */
+    void subwayAddTrain (Subway subway, ArrayList<Train> trainList);
 
-    void subwayAddTrain(Subway subway, Train train);
+    /**
+     * Agrega varias líneas a la red de metro
+     *
+     * @param subway    red de metro a la que se agregarán las líneas
+     * @param lineList lista de líneas que se agregarán al metro
+     */
+    void subwayAddLine (Subway subway, ArrayList<Line> lineList);
+
+    /**
+     * Agrega varios conductores a la red de metro
+     *
+     * @param subway    red de metro a la que se agregarán los conductores
+     * @param driverList lista de conductores que se agregarán al metro
+     */
+    void subwayAddDriver (Subway subway, ArrayList<Driver> driverList);
 }
